bcca4198cbb1fa944b6110632678fdc80f72c795
Jeremy Castagno <jeremybyu@gmail.com>
2020-04-07 11:46:50 -0400

    Faster Mesh Smoothing, Faster Polylidar

--- polylidar_plane_benchmark/scripts/visualize.py
+++ polylidar_plane_benchmark/scripts/visualize.py
@@ -12,7 +12,7 @@ from polylidar_plane_benchmark import (DEFAULT_PPB_FILE, DEFAULT_PPB_FILE_SECOND
 from polylidar_plane_benchmark.utility.o3d_util import create_open_3d_pcd, plot_meshes, get_arrow, create_open_3d_mesh, flatten
 from polylidar_plane_benchmark.utility.helper import (load_pcd_file, create_mesh_from_organized_point_cloud,
                                                       extract_all_dominant_plane_normals, create_meshes, load_pcd_and_meshes,
-                                                      extract_planes_and_polygons_from_mesh)
+                                                      extract_planes_and_polygons_from_mesh, tab40, create_open_3d_pcd)
 
 
 import click
@@ -28,10 +28,16 @@ def visualize():
 @click.option('-i', '--input-file', type=click.Path(exists=True), default=DEFAULT_PPB_FILE)
 @click.option('-s', '--stride', type=int, default=2)
 @click.option('-l', '--loops', type=int, default=20)
-def pcd(input_file: str, stride, loops):
+@click.option('--llambda', type=float, default=1.0)
+def pcd(input_file: str, stride, loops, llambda):
     """Visualize PCD File"""
-    pc_raw, pcd_raw, tri_mesh, tri_mesh_o3d = load_pcd_and_meshes(input_file, stride, loops)
+    pc_raw, pc_image = load_pcd_file(input_file, stride)
 
+    # Get just the points, no intensity
+    pc_points = np.ascontiguousarray(pc_raw[:, :3])
+    # Create Open3D point cloud
+    cmap = tab40()
+    pcd_raw = create_open_3d_pcd(pc_raw[:, :3], pc_raw[:, 3], cmap=cmap)
     plot_meshes([pcd_raw])
 
 
@@ -39,9 +45,10 @@ def pcd(input_file: str, stride, loops):
 @click.option('-i', '--input-file', type=click.Path(exists=True), default=DEFAULT_PPB_FILE)
 @click.option('-s', '--stride', type=int, default=2)
 @click.option('-l', '--loops', type=int, default=20)
-def mesh(input_file: str, stride, loops):
+@click.option('--llambda', type=float, default=1.0)
+def mesh(input_file: str, stride, loops, llambda):
     """Visualize Mesh from PCD File"""
-    pc_raw, pcd_raw, tri_mesh, tri_mesh_o3d = load_pcd_and_meshes(input_file, stride, loops)
+    pc_raw, pcd_raw, tri_mesh, tri_mesh_o3d = load_pcd_and_meshes(input_file, stride, loops, llambda)
 
     # Write Smoothed Mesh to File, debugging purposes
     output_file = str(input_file).replace(str(SYNPEB_DIR), str(SYNPEB_MESHES_DIR))
@@ -58,9 +65,10 @@ def mesh(input_file: str, stride, loops):
 @click.option('-i', '--input-file', type=click.Path(exists=True), default=DEFAULT_PPB_FILE)
 @click.option('-s', '--stride', type=int, default=2)
 @click.option('-l', '--loops', type=int, default=20)
-def ga(input_file, stride, loops):
+@click.option('--llambda', type=float, default=1.0)
+def ga(input_file, stride, loops, llambda):
     """Visualize Gaussian Accumulator File"""
-    pc_raw, pcd_raw, tri_mesh, tri_mesh_o3d = load_pcd_and_meshes(input_file, stride, loops)
+    pc_raw, pcd_raw, tri_mesh, tri_mesh_o3d = load_pcd_and_meshes(input_file, stride, loops, llambda)
     avg_peaks, pcd_all_peaks, arrow_avg_peaks, colored_icosahedron = extract_all_dominant_plane_normals(tri_mesh)
 
     # arrow = get_arrow(origin=[0,0,0], end=[3, 0, 0], cylinder_radius=0.01)
@@ -71,9 +79,10 @@ def ga(input_file, stride, loops):
 @click.option('-i', '--input-file', type=click.Path(exists=True), default=DEFAULT_PPB_FILE)
 @click.option('-s', '--stride', type=int, default=2)
 @click.option('-l', '--loops', type=int, default=20)
-def polygons(input_file, stride, loops):
+@click.option('--llambda', type=float, default=1.0)
+def polygons(input_file, stride, loops, llambda):
     """Visualize Polygon Extraction File"""
-    pc_raw, pcd_raw, tri_mesh, tri_mesh_o3d = load_pcd_and_meshes(input_file, stride, loops)
+    pc_raw, pcd_raw, tri_mesh, tri_mesh_o3d = load_pcd_and_meshes(input_file, stride, loops, llambda)
     avg_peaks, pcd_all_peaks, arrow_avg_peaks, colored_icosahedron = extract_all_dominant_plane_normals(tri_mesh)
     _, _, all_poly_lines = extract_planes_and_polygons_from_mesh(tri_mesh, avg_peaks)
     mesh_3d_polylidar = []
@@ -86,9 +95,10 @@ def polygons(input_file, stride, loops):
 @click.option('-v', '--variance', type=click.Choice(['0', '1', '2', '3', '4']), default='1')
 @click.option('-d', '--data', default="train")
 @click.option('-s', '--stride', type=int, default=2)
-@click.option('-l', '--loops', type=int, default=20)
+@click.option('-l', '--loops', type=int, default=10)
+@click.option('--llambda', type=float, default=1.0)
 @click.pass_context
-def polygons_all(ctx, variance, data, stride, loops):
+def polygons_all(ctx, variance, data, stride, loops, llambda):
     """Visualize Polygon Extraction from training/testing/gt set"""
     if int(variance) == 0:
         base_dir = SYNPEB_DIR_TRAIN_GT if data == "train" else SYNPEB_DIR_TEST_GT
@@ -96,8 +106,12 @@ def polygons_all(ctx, variance, data, stride, loops):
         base_dir = SYNPEB_DIR_TRAIN_ALL[int(
             variance) - 1] if data == "train" else SYNPEB_DIR_TEST_ALL[int(variance) - 1]
 
-    for fname in SYNPEB_ALL_FNAMES:
+    all_fnames = SYNPEB_ALL_FNAMES
+    if int(variance) != 0:
+        all_fnames = all_fnames[0:10]
+
+    for fname in all_fnames:
         fpath = str(base_dir / fname)
         logger.info("File: %s; stride=%d, loops=%d", fpath, stride, loops)
-        ctx.invoke(polygons, input_file=fpath, stride=stride, loops=loops)
+        ctx.invoke(polygons, input_file=fpath, stride=stride, loops=loops, llambda=llambda)
 
--- polylidar_plane_benchmark/utility/helper.py
+++ polylidar_plane_benchmark/utility/helper.py
@@ -1,6 +1,7 @@
 import random
 import time
 import warnings
+import gc
 warnings.filterwarnings("ignore", message="Optimal rotation is not uniquely or poorly defined ")
 
 import numpy as np
@@ -8,6 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import pypcd.pypcd as pypcd
 from scipy.spatial.transform import Rotation as R
+import open3d as o3d
 
 from polylidar import extract_point_cloud_from_float_depth, extract_tri_mesh_from_organized_point_cloud, MatrixDouble, Polylidar3D
 from polylidar.polylidarutil.open3d_util import construct_grid, create_lines, flatten
@@ -20,22 +22,28 @@ from fastga import GaussianAccumulatorS2, MatX3d, IcoCharts
 from fastga.peak_and_cluster import find_peaks_from_ico_charts
 from fastga.o3d_util import get_arrow, get_pc_all_peaks, get_arrow_normals
 
+import organizedpointfilters as opf
+import organizedpointfilters.cuda as opf_cuda
+from organizedpointfilters import Matrix3f, Matrix3fRef
+
 
 # Set the random seeds for determinism
 random.seed(0)
 np.random.seed(0)
+
 
 def tab40():
     """A discrete colormap with 40 unique colors"""
     colors_ = np.vstack([plt.cm.tab20c.colors, plt.cm.tab20b.colors])
     return colors.ListedColormap(colors_)
 
-def load_pcd_and_meshes(input_file, stride=2, loops=5):
+
+def load_pcd_and_meshes(input_file, stride=2, loops=5, _lambda=0.5):
     """Load PCD and Meshes
     """
-    pc_raw, depth_image = load_pcd_file(input_file, stride)
+    pc_raw, pc_image = load_pcd_file(input_file, stride)
     logger.info("Visualizing Point Cloud - Size: %dX%d ; # Points: %d",
-                depth_image.shape[0], depth_image.shape[1], pc_raw.shape[0])
+                pc_image.shape[0], pc_image.shape[1], pc_raw.shape[0])
 
     # Get just the points, no intensity
     pc_points = np.ascontiguousarray(pc_raw[:, :3])
@@ -43,7 +51,8 @@ def load_pcd_and_meshes(input_file, stride=2, loops=5):
     cmap = tab40()
     pcd_raw = create_open_3d_pcd(pc_raw[:, :3], pc_raw[:, 3], cmap=cmap)
 
-    tri_mesh, tri_mesh_o3d = create_meshes(pc_points, stride=stride, loops=loops)
+    # tri_mesh, tri_mesh_o3d = create_meshes(pc_points, stride=stride, loops=loops)
+    tri_mesh, tri_mesh_o3d = create_meshes_cuda(pc_image, loops=loops, _lambda=_lambda)
 
     return pc_raw, pcd_raw, tri_mesh, tri_mesh_o3d
 
@@ -63,15 +72,15 @@ def filter_and_create_open3d_polygons(points, polygons, rm=None, line_radius=0.0
 
 
 def extract_planes_and_polygons_from_mesh(tri_mesh, avg_peaks,
-                                polylidar_kwargs=dict(alpha=0.0, lmax=0.20, min_triangles=100,
-                                                      z_thresh=0.02, norm_thresh=0.98, norm_thresh_min=0.95, min_hole_vertices=6)):
+                                          polylidar_kwargs=dict(alpha=0.0, lmax=0.20, min_triangles=100,
+                                                                z_thresh=0.02, norm_thresh=0.98, norm_thresh_min=0.98, min_hole_vertices=6, task_threads=4)):
 
     pl = Polylidar3D(**polylidar_kwargs)
     avg_peaks_mat = MatrixDouble(avg_peaks)
     t0 = time.perf_counter()
-    all_planes, all_polygons = pl.extract_planes_and_polygons(tri_mesh, avg_peaks_mat)
+    all_planes, all_polygons = pl.extract_planes_and_polygons_optimized(tri_mesh, avg_peaks_mat)
     t1 = time.perf_counter()
-    
+
     polylidar_time = (t1 - t0) * 1000
     logger.info("Polygon Extraction - Took (ms): %.2f", polylidar_time)
     vertices = np.asarray(tri_mesh.vertices)
@@ -113,7 +122,7 @@ def get_image_peaks(ico_chart, ga, level=2, **kwargs):
     return avg_peaks, pcd_all_peaks, arrow_avg_peaks
 
 
-def down_sample_normals(triangle_normals, ds=4, min_samples=10000, flip_normals=True, **kwargs):
+def down_sample_normals(triangle_normals, ds=4, min_samples=10000, flip_normals=False, **kwargs):
     num_normals = triangle_normals.shape[0]
     ds_normals = int(num_normals / ds)
     to_sample = max(min([num_normals, min_samples]), ds_normals)
@@ -139,7 +148,8 @@ def extract_all_dominant_plane_normals(tri_mesh, level=5, **kwargs):
     gaussian_normals = np.asarray(ga.get_bucket_normals())
     accumulator_counts = np.asarray(ga.get_normalized_bucket_counts())
 
-    logger.info("Gaussian Accumulator - Normals Sampled: %d; Took (ms): %.2f", triangle_normals_ds.shape[0], (t2 - t1) * 1000)
+    logger.info("Gaussian Accumulator - Normals Sampled: %d; Took (ms): %.2f",
+                triangle_normals_ds.shape[0], (t2 - t1) * 1000)
 
     # Visualize the Sphere
     refined_icosahedron_mesh = create_open_3d_mesh(np.asarray(ga.mesh.triangles), np.asarray(ga.mesh.vertices))
@@ -152,16 +162,19 @@ def extract_all_dominant_plane_normals(tri_mesh, level=5, **kwargs):
     return avg_peaks, pcd_all_peaks, arrow_avg_peaks, colored_icosahedron
 
 
-def create_meshes(pc_points, stride=2, loops=5):
+def create_meshes(pc_points, stride=2, loops=5, _lambda=0.5,):
     tri_mesh = create_mesh_from_organized_point_cloud(pc_points, stride=stride)
-    tri_mesh_o3d = create_open_3d_mesh(np.asarray(tri_mesh.triangles), pc_points)
 
+    tri_mesh_o3d = create_open_3d_mesh(np.asarray(tri_mesh.triangles), pc_points)
     # Perform Smoothing
+    filter_scope = o3d.geometry.FilterScope(0)
+    kwargs = dict(filter_scope=filter_scope)
+    kwargs['lambda'] = _lambda
     t1 = time.perf_counter()
-    if loops > 1:
+    if loops > 0:
         tri_mesh_o3d = tri_mesh_o3d.filter_smooth_laplacian(loops)
     t2 = time.perf_counter()
-    tri_mesh_o3d.compute_triangle_normals()
+    tri_mesh_o3d = tri_mesh_o3d.compute_triangle_normals()
 
     tri_mesh = open_3d_mesh_to_tri_mesh(tri_mesh_o3d)
 
@@ -170,8 +183,77 @@ def create_meshes(pc_points, stride=2, loops=5):
     return tri_mesh, tri_mesh_o3d
 
 
+def create_mesh_from_organized_point_cloud_with_o3d(pcd: np.ndarray, rows=500, cols=500, stride=2):
+    pcd_ = pcd
+    if pcd.ndim == 3:
+        rows = pcd.shape[0]
+        cols = pcd.shape[1]
+        stride = 1
+        pcd_ = pcd.reshape((rows * cols, 3))
+
+    pcd_mat = MatrixDouble(pcd_, copy=True)
+    pcd_mat_np = np.asarray(pcd_mat)
+    tri_mesh = extract_tri_mesh_from_organized_point_cloud(pcd_mat, rows, cols, stride)
+
+    tri_mesh_o3d = create_open_3d_mesh(np.asarray(tri_mesh.triangles), pcd_)
+
+    return tri_mesh, tri_mesh_o3d
+
+
+def laplacian_opc(opc, loops=5, _lambda=0.5, kernel_size=3):
+    opc_float = (np.ascontiguousarray(opc[:, :, :3])).astype(np.float32)
+
+    a_ref = Matrix3fRef(opc_float)
+
+    t1 = time.perf_counter()
+    b_cp = opf.kernel.laplacian_K3(a_ref, _lambda=_lambda, iterations=loops)
+    t2 = time.perf_counter()
+    logger.info("OPC Mesh Smoothing Took (ms): %.2f", (t2 - t1) * 1000)
+
+    opc_float_out = np.asarray(b_cp)
+    opc_out = opc_float_out.astype(np.float64)
+
+    return opc_out
+
+
+def laplacian_opc_cuda(opc, loops=5, _lambda=0.5, **kwargs):
+
+    opc_float = (np.ascontiguousarray(opc[:, :, :3])).astype(np.float32)
+
+    t1 = time.perf_counter()
+    opc_float_out = opf_cuda.kernel.laplacian_K3_cuda(opc_float, loops=loops, _lambda=_lambda, **kwargs)
+    t2 = time.perf_counter()
+
+    logger.info("OPC CUDA Laplacian Mesh Smoothing Took (ms): %.2f", (t2 - t1) * 1000)
+
+    # only for visualization purposes here
+    opc_out = opc_float_out.astype(np.float64)
+
+    return opc_out
+
+
+def create_meshes_cuda(opc, loops=5, _lambda=0.5):
+    """Creates a mesh from a noisy organized point cloud
+
+    Arguments:
+        opc {ndarray} -- Must be MXNX3
+
+    Keyword Arguments:
+        loops {int} -- How many loop iterations (default: {5})
+        _lambda {float} -- weighted iteration movement (default: {0.5})
+
+    Returns:
+        [tuple(mesh, o3d_mesh)] -- polylidar mesh and o3d mesh reperesentation
+    """
+    smooth_opc = laplacian_opc_cuda(opc, loops=loops, _lambda=_lambda)
+    tri_mesh, tri_mesh_o3d = create_mesh_from_organized_point_cloud_with_o3d(smooth_opc)
+
+    return tri_mesh, tri_mesh_o3d
+
+
 def create_mesh_from_organized_point_cloud(pcd, rows=500, cols=500, stride=2):
     pcd_mat = MatrixDouble(pcd)
+    pcd_mat_np = np.asarray(pcd_mat)
     tri_mesh = extract_tri_mesh_from_organized_point_cloud(pcd_mat, rows, cols, stride)
     return tri_mesh
 
@@ -190,43 +272,14 @@ def load_pcd_file(fpath, stride=2):
     pc_np = np.column_stack((x, y, z, i))
     # Image Point Cloud (organized)
     pc_np_image = np.reshape(pc_np, (width, height, 4))
-
+    # I'm expecting the "image" to have the rows/y-axis going down
+    pc_np_image = np.ascontiguousarray(np.flip(pc_np_image, 0))
     if stride is not None:
         pc_np_image = pc_np_image[::stride, ::stride, :]
         total_points_ds = pc_np_image.shape[0] * pc_np_image.shape[1]
         pc_np = np.reshape(pc_np_image, (total_points_ds, 4))
 
-    depth_image = np.ascontiguousarray(pc_np_image[:, :, 0])
+    pc_np_image = pc_np_image.astype(np.float64)
     pc_np = pc_np.astype(np.float64)
 
-    return pc_np, depth_image
-
-# def load_pcd_file(fpath, ds=None):
-#     pc = pypcd.PointCloud.from_path(fpath)
-#     x = pc.pc_data['x']
-#     y = pc.pc_data['y']
-#     z = pc.pc_data['z']
-#     i = pc.pc_data['intensity']
-
-#     width = int(pc.get_metadata()['width'])
-#     height = int(pc.get_metadata()['height'])
-
-#     # Flat Point Cloud
-#     pc_np = np.column_stack((x, y, z, i))
-#     # Image Point Cloud (organized)
-#     pc_np_image = np.reshape(pc_np, (width, height, 4))
-#     depth_image = np.ascontiguousarray(pc_np_image[:, :, 0])
-
-#     if ds is not None:
-#         pc_np_image = pc_np_image[::ds, ::ds, :]
-#         total_points_ds = pc_np_image.shape[0] * pc_np_image.shape[1]
-#         pc_np = np.reshape(pc_np_image, (total_points_ds, 4))
-
-#     intrinsics = get_intrinsic_matrix(depth_image)
-#     # intrinsics, distortion, rotation_matrix, tranlsation  = estimate_intrinsics_matrix(depth_image, pc_np_image)
-#     # rm = R.from_matrix(rotation_matrix)
-
-#     pc_from_depth = depth_to_pc(depth_image, intrinsics, ds, distortion=None, rm=None)
-#     pc_from_depth_rotated = pc_from_depth
-
-#     return pc_np, pc_from_depth_rotated, depth_image
+    return pc_np, pc_np_image
